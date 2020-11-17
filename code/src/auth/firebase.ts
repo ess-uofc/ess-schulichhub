1d63633512d4c8f03108d01538d9288f39a7ab26
= <m.m200@hotmail.co.uk>
2020-11-17 16:55:32 -0700

    firebasefunctions/med/firebase

--- code/src/auth/firebase.ts
+++ code/src/auth/firebase.ts
@@ -14,13 +14,32 @@ const firebaseConfig = {
 
 const app = firebase.initializeApp(firebaseConfig)
 export default app
-export async function signUpUser(first:string,last:string, email:string,password:string){
-    const res = await app.auth().createUserWithEmailAndPassword(email,password)
 
+export async function signUpUser(first:string,last:string, email:string,password:string){
+
+    //Creates a user in firebase using email and password auth
+    //returns a string with error code in sentence case
+    //
+
+    try{
+        const res = await app.auth().createUserWithEmailAndPassword(email,password)
+        if (res){
+            return res.user
+        }else{
+            return null //
+        }
+    }catch(e){
+        return parseError(e.code)
+    }
+    
     
 }
 
-async function getUserDetails(uid:string){
+async function getUserDetails(uid:string):Promise<User|undefined>{
+    //get's user information from firebase
+    // returns Promise with User model if transaction completed
+    // TODO Add props to list and iterate instead of getting each 
+    // field line by line?
     const doc = await app.firestore().collection('users').doc(uid).get()
     const firstName = await doc.get("firstName")
     const lastName = await doc.get("lastName")
@@ -32,10 +51,25 @@ async function getUserDetails(uid:string){
     }
 }
 
+async function addUserToDB(uid:string,firstName:string,lastName:string){
+    //Adds the user to the users colleciton 
+    
+    const doc = await app.firestore().collection('users').doc(uid).set(
+        {
+            firstName:firstName,
+            lastName:lastName,
+            //TOOD add other fields
+        }
+    )
+
+}
+
+
 class User{
     firstName:string
     lastName:string
     uid:string
+    // TODO add other attributes
     constructor(firstName:string, lastName:string,uid:string){
         this.firstName = firstName
         this.lastName =lastName
@@ -44,4 +78,10 @@ class User{
     signUp(){
         // Todo Implement signup as a class mehtod??
     }
+}
+
+function parseError(error:string){
+    //example error auth/email-already-exists to Email Already Exists
+    // Could be used to communicate with user in front end
+    return error.split('/')[1].split('-').map((e)=>(e.charAt(0).toUpperCase()+e.slice(1)))
 }
