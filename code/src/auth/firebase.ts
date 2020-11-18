7b06cfed56fc57d6efe5cf5980651fb39838c8c6
= <m.m200@hotmail.co.uk>
2020-11-17 19:40:26 -0700

    Added .env to .gitignore

--- code/src/auth/firebase.ts
+++ code/src/auth/firebase.ts
@@ -1,5 +1,4 @@
 import firebase from "firebase"
-import { env } from "process";
 
 
 const firebaseConfig = {
@@ -8,7 +7,7 @@ const firebaseConfig = {
     databaseURL: process.env.REACT_APP_PROJECTID,
     projectId: process.env.REACT_APP_PROJECTID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEEASURMENT_ID 
   };
