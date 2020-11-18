809aa7732cb04f6a56ee2770f50c54298ebe1f2a
Ratik Kapoor <ratikrk@gmail.com>
2020-11-17 23:13:54 -0700

    Test commit for CI/CD

--- code/src/pages/Tab1.tsx
+++ code/src/pages/Tab1.tsx
@@ -17,7 +17,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <ExploreContainer name="Tab 1 page, Ratik says hello!" />
       </IonContent>
     </IonPage>
   );
