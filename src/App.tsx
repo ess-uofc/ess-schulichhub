d1ca0891537e25d5759c692ba8b700aea76133c1
MrClean-1 <Cjdrewes@gmail.com>
2020-11-24 23:59:14 -0700

    Login methods & prettier fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Login from './pages/Login';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,6 +35,7 @@ const App: React.FC = () => (
                     <Route path="/tab1" component={Tab1} exact={true} />
                     <Route path="/tab2" component={Tab2} exact={true} />
                     <Route path="/tab3" component={Tab3} />
+                    <Route path="/login" component={Login} exact={true} />
                     <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
                 </IonRouterOutlet>
                 <IonTabBar slot="bottom">
@@ -48,6 +50,10 @@ const App: React.FC = () => (
                     <IonTabButton tab="tab3" href="/tab3">
                         <IonIcon icon={square} />
                         <IonLabel>Tab 3</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="login" href="/login">
+                        <IonIcon icon={square} />
+                        <IonLabel>Login</IonLabel>
                     </IonTabButton>
                 </IonTabBar>
             </IonTabs>
