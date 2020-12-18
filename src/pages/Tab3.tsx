4ad7dd98ab357705cd24920aa6cdb2c3228c587d
Dennis <lieudennis7@gmail.com>
2020-12-17 18:50:50 -0700

    Added basic navbar, removed tabs

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -8,6 +8,9 @@ const Tab3: React.FC = () => {
         <IonPage>
             <IonHeader>
                 <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton auto-hide="false"> Menu </IonMenuButton>
+                    </IonButtons>
                     <IonTitle>Tab 3</IonTitle>
                 </IonToolbar>
             </IonHeader>
