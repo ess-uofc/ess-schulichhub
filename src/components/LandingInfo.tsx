f9a0e818f5d4617502bfa3e9e17bf1989aabea59
Dennis <lieudennis7@gmail.com>
2021-01-29 12:37:28 -0700

    Removed html tags from LandingInfo.tsx

--- src/components/LandingInfo.tsx
+++ src/components/LandingInfo.tsx
@@ -1,53 +1,93 @@
 import React from 'react';
 import './LandingInfo.scss';
-import { IonButton } from '@ionic/react';
+import { IonButton, IonGrid, IonRow, IonCol, IonText, IonHeader, IonImg } from '@ionic/react';
 
 const LandingInfo: React.FC = () => {
     return (
-        <div>
-            <h1 className="ion-margin">Connect with clubs and find opportunitites to get involved</h1>
-            <div className="ion-margin landingParagraphs">
-                <h3>Project Hub</h3>
-                <p>
-                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-                    magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
-                </p>
-                <IonButton className="learnMore">Learn More</IonButton>
-            </div>
-            <div className="ion-margin landingParagraphs">
-                <h3>Volunteer Hub</h3>
-                <p>
-                    Amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
-                </p>
-                <IonButton className="learnMore">Learn More</IonButton>
-            </div>
-            <div className="ion-margin landingParagraphs">
-                <h3>Startup Hub</h3>
-                <p>
-                    Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-                    dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam.
-                </p>
-                <IonButton className="learnMore">Learn More</IonButton>
-            </div>
-            <div className="ion-margin landingParagraphs">
-                <h3>Club Listings</h3>
-                <p>
-                    Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
-                    sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.
-                </p>
-                <IonButton className="learnMore">Learn More</IonButton>
-            </div>
-            <h2>You&apos;re in good company</h2>
-            <div className="placeholderBlock">Placeholder</div>
-            <div className="placeholderBlock">Placeholder</div>
-            <div className="placeholderBlock">Placeholder</div>
-            <div className="placeholderBlock">Placeholder</div>
-            <div className="placeholderBlock">Placeholder</div>
-            <div className="placeholderBlock">Placeholder</div>
-            <div className="placeholderBlock">Placeholder</div>
-            <div className="placeholderBlock">Placeholder</div>
-        </div>
+        <IonGrid>
+            <IonRow>
+                <IonHeader className="ion-margin">Connect with clubs and find opportunitites to get involved</IonHeader>
+            </IonRow>
+            <IonRow>
+                <IonCol>
+                    <IonText className="ion-margin">Project Hub</IonText>
+                </IonCol>
+                <IonCol>
+                    <IonText className="ion-margin">Volunteer Hub</IonText>
+                </IonCol>
+            </IonRow>
+            <IonRow>
+                <IonCol>
+                    <IonText>
+                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                        consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+                    </IonText>
+                    <IonButton className="learnMore">Learn More</IonButton>
+                </IonCol>
+                <IonCol>
+                    <IonText>
+                        Amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
+                    </IonText>
+                    <IonButton className="learnMore">Learn More</IonButton>
+                </IonCol>
+            </IonRow>
+            <IonRow>
+                <IonCol>
+                    <IonText className="ion-margin">Startup Hub</IonText>
+                </IonCol>
+                <IonCol>
+                    <IonText className="ion-margin">Club Listings</IonText>
+                </IonCol>
+            </IonRow>
+            <IonRow>
+                <IonCol>
+                    <IonText>
+                        Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+                        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam.
+                    </IonText>
+                    <IonButton className="learnMore">Learn More</IonButton>
+                </IonCol>
+                <IonCol>
+                    <IonText>
+                        Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
+                        dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.
+                    </IonText>
+                    <IonButton className="learnMore">Learn More</IonButton>
+                </IonCol>
+            </IonRow>
+            <IonRow>
+                <IonHeader>You&apos;re in good company</IonHeader>
+            </IonRow>
+            <IonRow>
+                <IonCol>
+                    <IonImg className="placeholderBlock" src="https://essucalgary.com/images/ess-logo.png"></IonImg>
+                </IonCol>
+                <IonCol>
+                    <IonImg className="placeholderBlock" src="https://essucalgary.com/images/ess-logo.png"></IonImg>
+                </IonCol>
+                <IonCol>
+                    <IonImg className="placeholderBlock" src="https://essucalgary.com/images/ess-logo.png"></IonImg>
+                </IonCol>
+                <IonCol>
+                    <IonImg className="placeholderBlock" src="https://essucalgary.com/images/ess-logo.png"></IonImg>
+                </IonCol>
+            </IonRow>
+            <IonRow>
+                <IonCol>
+                    <IonImg className="placeholderBlock" src="https://essucalgary.com/images/ess-logo.png"></IonImg>
+                </IonCol>
+                <IonCol>
+                    <IonImg className="placeholderBlock" src="https://essucalgary.com/images/ess-logo.png"></IonImg>
+                </IonCol>
+                <IonCol>
+                    <IonImg className="placeholderBlock" src="https://essucalgary.com/images/ess-logo.png"></IonImg>
+                </IonCol>
+                <IonCol>
+                    <IonImg className="placeholderBlock" src="https://essucalgary.com/images/ess-logo.png"></IonImg>
+                </IonCol>
+            </IonRow>
+        </IonGrid>
     );
 };
 
