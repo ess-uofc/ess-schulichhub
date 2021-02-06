bba8df66e87cbe5a441bd6a08bd3952e18be7071
Mohamad Abdel Rida <m.m2000@hotmail.co.uk>
2021-02-06 15:16:56 -0700

    Added Private Routes

--- src/app/routing.tsx
+++ src/app/routing.tsx
@@ -13,3 +13,42 @@ export default function redirectAfterAuthEvent(target: string): void {
         unAuthedRoutes.includes(window.location.pathname) ? (location.href = target) : console.log('Already at target');
     }
 }
+
+import React, { useState } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import app from '../Models/firebase';
+import PropTypes from 'prop-types';
+
+export const PrivateRoute: React.FC<{
+    /**
+     * @author Mohamad Abdel Rida
+     * @param component -> The protected Component being routed to
+     * @param path the path corresponding to the protected component
+     * @param exact A boolean flag that toggles exact path matching
+     */
+    component: React.FC;
+    path: string;
+    exact: boolean;
+}> = (props) => {
+    const [isAuthed, setIsAuthed] = useState<boolean | null>(null);
+    app.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            setIsAuthed(true);
+        } else {
+            setIsAuthed(false);
+        }
+    });
+    // unsubscribe();
+    console.log(isAuthed);
+    return isAuthed ? (
+        <Route path={props.path} exact={props.exact} component={props.component} />
+    ) : (
+        <Redirect to="/login" exact={true} />
+    );
+};
+
+PrivateRoute.propTypes = {
+    component: PropTypes.func.isRequired,
+    path: PropTypes.string.isRequired,
+    exact: PropTypes.bool.isRequired,
+};
