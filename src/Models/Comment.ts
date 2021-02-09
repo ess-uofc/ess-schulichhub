5cb4e0122b506e896a1a28e828281115d480ba4e
Robert Brown <brown.rjb75@gmail.com>
2021-02-08 17:41:33 -0700

    added comment class

---
+++ src/Models/Comment.ts
@@ -0,0 +1,32 @@
+import User from "./User";
+
+export default class Comment {
+    /**
+     * Comment class for holding comments
+     * @author Robert Brown
+     * @since 0.0.4
+     */
+
+    id: number;
+    content: string;
+    timestamp: Date;
+    replyTo?: Comment;
+    user?: User;
+
+    /**
+     * default constructor for the comment class
+     * @param id comment identifier
+     * @param content content of the comment
+     * @param timestamp time the comment was posted
+     * @param replyTo (optional) if the comment is in reply, the parent comment
+     * @param user (optional for development) user who posted the comment
+     */
+    constructor(id: number, content: string, timestamp: Date, replyTo?: Comment, user?: User) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+        this.replyTo = replyTo;
+        this.user = user;
+    }
+
+}
