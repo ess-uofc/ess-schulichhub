4720657e78fb7e0c9961c32468bc26d7c7c9e6a8
Mohamad Abdel Rida <m.m2000@hotmail.co.uk>
2021-02-08 17:55:54 -0700

    Updated DocTypes

--- src/Models/DocTypes.ts
+++ src/Models/DocTypes.ts
@@ -1,19 +1,22 @@
 export interface UserDoc {
     firstName: string;
     lastName: string;
+    [key: string]: any;
 
     //[key: string]: any;
 
     // Add other user fields here
 }
-export interface PostDoc { 
-    title: string,
-    content: string,
-    [key: string]: any,
-    
+export interface PostDoc {
+    title: string;
+    content: string;
+    uid: string;
+    comments: Array<string>;
+    [key: string]: any;
 }
 export interface CommentDoc {
-    title: string;
+    replyTo: string;
+    uid: string;
     content: string;
     [key: string]: any;
 }
