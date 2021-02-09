1ac81fb6281ccbbf3a84e85ad6eb8b65ec00542b
Mohamad Abdel Rida <m.m2000@hotmail.co.uk>
2021-02-08 20:16:28 -0700

    fixed Types and Added  query to home view
    fixed Post and Query types.

--- src/pages/PostView.tsx
+++ src/pages/PostView.tsx
@@ -27,8 +27,6 @@ import { PostCategory } from '../Models/Enums';
 const testPost = new Post(
     'Test',
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut mauris purus. Nunc ut lorem nec est accumsan luctus. Proin rhoncus, dolor sit amet vehicula elementum, turpis nulla sagittis velit, at auctor est urna vehicula libero. Phasellus facilisis neque non euismod rhoncus. Phasellus ipsum erat, varius id tellus a, blandit posuere elit. Donec sagittis mauris tristique urna egestas, eget placerat ligula sodales. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ut eleifend dolor. Donec non dolor laoreet, malesuada eros ut, aliquam nu',
-    1612037497,
-    PostCategory.Discussion,
 );
 
 const PostView: React.FC = () => {
