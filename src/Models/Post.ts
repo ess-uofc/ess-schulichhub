c7ae063dce413a3880f9493a2c39afd52ddaa002
Ratik Kapoor <ratikrk@gmail.com>
2021-02-09 12:26:18 -0700

    Add PostCategory to Post class

--- src/Models/Post.ts
+++ src/Models/Post.ts
@@ -1,4 +1,6 @@
 import User from './User';
+import { PostCategory } from './Enums';
+import PostAttachment from './PostAttachment';
 
 /**
  * Post class for holding information about posts
@@ -8,19 +10,33 @@ export default class Post {
     content: string;
     title: string;
     timestamp: number;
+    category: PostCategory;
+    attachment?: PostAttachment;
     user?: User;
 
     /**
-     * default constructor
-     * @param title title of the post
-     * @param user user who created the post
-     * @param timestamp time the post was created
-     * @param content content of the post
+     * Default constructor
+     *
+     * @param title - Title of the post
+     * @param user - User who created the post TODO: Make mandatory
+     * @param timestamp - Time the post was created
+     * @param content - Content of the post
+     * @param category - Category of the post
+     * @param attachment - Optional attachment for the post
      */
-    constructor(title: string, content: string, timestamp: number, user?: User) {
+    constructor(
+        title: string,
+        content: string,
+        timestamp: number,
+        category: PostCategory,
+        user?: User,
+        attachment?: PostAttachment,
+    ) {
         this.title = title;
         this.user = user;
         this.timestamp = timestamp;
         this.content = content;
+        this.category = category;
+        this.attachment = attachment;
     }
 }
