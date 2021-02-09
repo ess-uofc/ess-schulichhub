05527b04513d5dd3416d2a42f46be48d8e030faf
Mohamad Abdel Rida <m.m2000@hotmail.co.uk>
2021-02-09 15:05:26 -0700

    Added method on Post to handle time from creation

--- src/Models/Post.ts
+++ src/Models/Post.ts
@@ -1,8 +1,9 @@
+import firebase from 'firebase';
 import User from './User';
 export default class Post {
     content: string;
     title: string;
-    timestamp?: firebase.default.firestore.Timestamp;
+    timestamp?: firebase.firestore.Timestamp;
     user?: User;
 
     /**
@@ -15,12 +16,38 @@ export default class Post {
      * @param category - Category of the post
      * @param attachment - Optional attachment for the post
      */
-    constructor(title: string, content: string, timestamp?: firebase.default.firestore.Timestamp, user?: User) {
+    constructor(title: string, content: string, timestamp?: firebase.firestore.Timestamp, user?: User) {
         this.title = title;
         this.user = user;
         this.timestamp = timestamp;
         this.content = content;
         this.category = category;
         this.attachment = attachment;
+    }
+    public getTimePosted(): string {
+        /**
+         * @author Mohamad Abdel Rida
+         * Returns a formatted string of when the post
+         * was created.
+         *
+         *
+         */
+        if (this.timestamp) {
+            const now = firebase.firestore.Timestamp.now();
+
+            const diff = now.seconds - this.timestamp.seconds;
+
+            if (diff / 86400 >= 1) {
+                return Math.ceil(diff / 86400) + ' days ago';
+            } else {
+                if (diff > 3600) {
+                    return Math.ceil(diff / 3600) + ' hours go';
+                } else {
+                    return Math.ceil(diff / 60) + '  minutes ago';
+                }
+            }
+        } else {
+            return 'Some time Somewhere in the cosmic web';
+        }
     }
 }
