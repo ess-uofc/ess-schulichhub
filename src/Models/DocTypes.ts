f84598f8336adb2516adcdb9bebc8fd747873df7
Mohamad Abdel Rida <m.m2000@hotmail.co.uk>
2021-02-21 23:43:31 -0700

    Added post filtering

--- src/Models/DocTypes.ts
+++ src/Models/DocTypes.ts
@@ -4,9 +4,9 @@ export interface UserDoc {
     firstName: string;
     lastName: string;
     uid: string;
+    major?: string,
+    
     [key: string]: any;
-
-    //[key: string]: any;
 
     // Add other user fields here
 }
--- src/components/HomePostView.tsx
+++ src/components/HomePostView.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './HomePostView.scss';
-import { IonContent } from '@ionic/react';
+import { IonContent, IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/react';
 import PostContainer from '../components/PostContainer';
 import Post from '../Models/Post';
 import FireStoreDB from '../Models/firestore';
@@ -10,43 +10,56 @@ import { PostCategory } from '../Models/Enums';
 const HomePostView: React.FC = () => {
     const [posts, setPosts] = useState<Array<{ id: string; data: PostDoc }>>();
     const db = new FireStoreDB();
-    async function getPosts() {
+    const [postCategory, SetPostCategory] = useState<PostCategory>();
+    function handleSnapshot(snapshot: firebase.default.firestore.QuerySnapshot) {
+        console.log('Updated');
+        const snapshots = snapshot.docs;
+        const docs = snapshots.map((docSnapshot) => {
+            return { id: docSnapshot.id, data: docSnapshot.data() as PostDoc };
+        });
+
+        setPosts(docs);
+    }
+    function getPosts() {
         /**
          * @author Mohamad ABdel Rida
          * Executes a query to get posts.
          * Gets posts that have a Timestamp
          *
          */
-        // const post = await db.query<PostDoc>('posts', 'timestamp', '!=', '');
-        const unsub = db.db
-            .collection('posts')
-            .orderBy('timestamp', 'desc')
-            .onSnapshot({
-                next: (snapshot: firebase.default.firestore.QuerySnapshot) => {
-                    console.log('Updated');
-                    const snapshots = snapshot.docs;
-                    const docs = snapshots.map((docSnapshot) => {
-                        return { id: docSnapshot.id, data: docSnapshot.data() as PostDoc };
-                    });
-
-                    setPosts(docs);
-                },
+        const postsCollection = db.db.collection('posts').orderBy('timestamp', 'desc');
+        if (postCategory) {
+            return postsCollection.where('category', '==', postCategory).onSnapshot({ next: handleSnapshot });
+        } else {
+            return postsCollection.onSnapshot({
+                next: handleSnapshot,
             });
-        return unsub;
+        }
     }
 
     useEffect(() => {
         const unsub = getPosts();
-        return () => {
-            unsub
-                .then((v) => v())
-                .catch((e) => {
-                    console.log(e);
-                });
-        };
-    }, [posts?.length]);
+        return unsub;
+    }, [posts?.length, postCategory]);
     return (
         <IonContent>
+            <IonItem>
+                <IonLabel>Post Category</IonLabel>
+                <IonSelect
+                    value={postCategory}
+                    okText="Okay"
+                    cancelText="Dismiss"
+                    onIonChange={(e) => SetPostCategory(e.detail.value as PostCategory)}
+                >
+                    {Object.keys(PostCategory).map((v, e) => {
+                        return (
+                            <IonSelectOption key={e} value={v}>
+                                {v}
+                            </IonSelectOption>
+                        );
+                    })}
+                </IonSelect>
+            </IonItem>
             {posts
                 ? posts.map((v, k) => {
                       return (
