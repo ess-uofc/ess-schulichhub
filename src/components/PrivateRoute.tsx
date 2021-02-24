603dfd037a982dcdc8c268081ceece2c3062a8c4
Mohamad Abdel Rida <m.m2000@hotmail.co.uk>
2021-02-23 19:13:20 -0700

    Fix: eslint

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import app from '../Models/firebase';
 import PropTypes from 'prop-types';
 import { loadingComponent } from './Loading';
