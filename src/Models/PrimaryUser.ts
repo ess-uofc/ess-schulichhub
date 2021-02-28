f391e2bcce904eb8f6adddf52d4b3850934dad85
Mohamad Abdel Rida <m.m2000@hotmail.co.uk>
2021-02-27 22:05:40 -0700

    Added more docs and verify email method

--- src/Models/PrimaryUser.ts
+++ src/Models/PrimaryUser.ts
@@ -3,13 +3,18 @@ import { FirebaseUser, Firestore } from './firebase';
 import User from './User';
 
 export default class PrimaryUser extends User {
-    private user?: FirebaseUser;
+    private user: FirebaseUser;
 
     constructor(user: FirebaseUser, details: UserDoc) {
         super(details);
         this.user = user;
     }
 
+    /**
+     * @param {FirebaseUser} user: firebase user
+     * create Primary User from Firebase User object
+     *
+     */
     static async fromUser(user: FirebaseUser): Promise<PrimaryUser | undefined> {
         try {
             const details = (await Firestore.collection('users').withConverter(User).doc(user.uid).get()).data();
@@ -17,10 +22,19 @@ export default class PrimaryUser extends User {
         } catch (e) {}
     }
 
+    /**
+     * delete just the firebase instance of the user
+     * doesn't delete any information associated with the user, such as user docs, comments posts, etc
+     */
     public async delete(): Promise<void> {
         await this.user?.delete();
     }
-    public async verifyEmail(): Promise<void> {
-        await this.user?.sendEmailVerification();
+
+    /**
+     * Verify user email
+     * @param {string} continueUrl where to send user after verification is completed
+     */
+    public async verifyEmail(continueUrl = 'https://hub.essucalgary.com/'): Promise<void> {
+        await this.user.sendEmailVerification({ url: continueUrl });
     }
 }
