b984baaf81ca1a21b1faabf04e3d61e531e072e2
MrClean-1 <Cjdrewes@gmail.com>
2021-03-09 18:07:29 -0700

    Added routing and basic page structure

---
+++ src/pages/ProfileView.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IonPage, IonContent, IonIcon } from '@ionic/react';
+import './PostView.scss';
+
+const ProfileView: React.FC = () => {
+    return (
+        <IonPage>
+            <IonContent>
+                <IonIcon />
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default ProfileView;
