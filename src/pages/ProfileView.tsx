ebba49b26eb1b045dc179f8491c75d330b71ef51
MrClean-1 <Cjdrewes@gmail.com>
2021-03-09 23:53:07 -0700

    Fixed a couple versioning issues and added a basic template

--- src/pages/ProfileView.tsx
+++ src/pages/ProfileView.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { IonPage, IonContent, IonIcon } from '@ionic/react';
-import './PostView.scss';
+import {IonPage, IonContent, IonImg, IonItem, IonText, IonButton} from '@ionic/react';
+import './ProfileView.scss';
 
 const ProfileView: React.FC = () => {
     return (
         <IonPage>
-            <IonContent>
-                <IonIcon />
+            <IonContent fullscreen>
+                <IonItem className="wrapper">
+                    <h4>Profile</h4>
+                </IonItem>
+                <IonImg className="videoPlaceholder" src="https://essucalgary.com/images/ess-logo.png"/>
+                <h2>Student Name</h2>
+                <IonText>Bio Goes Here !!</IonText>
+                <div/>
+                <IonButton className="button">Edit Profile</IonButton>
+                <div className="bottomOfPage"/>
+                <IonItem>Previous Posts:</IonItem>
+                <IonContent />
+                <IonText>[LOGO] SchulichHub</IonText>
+                <IonItem className="footer ion-margin" color="secondary"></IonItem>
+                <IonText className="bottomOfPage">© ESS Schulich School of Engineering U of C, 2020</IonText>
             </IonContent>
         </IonPage>
     );
