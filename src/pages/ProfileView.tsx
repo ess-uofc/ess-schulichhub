de1626b535ecb5c1db1add192bdc6cacfe9a2874
MrClean-1 <Cjdrewes@gmail.com>
2021-03-15 22:18:11 -0600

    Other misc changes

--- src/pages/ProfileView.tsx
+++ src/pages/ProfileView.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {IonPage, IonContent, IonImg, IonItem, IonText, IonButton} from '@ionic/react';
 import './ProfileView.scss';
 
+const bio = 'University of Calgary - ESS LOVER \n This is the Bio \n It supports multiline stuff';
+const name = 'Student Name';
+
 const ProfileView: React.FC = () => {
     return (
         <IonPage>
@@ -10,15 +13,18 @@ const ProfileView: React.FC = () => {
                     <h4>Profile</h4>
                 </IonItem>
                 <IonImg className="videoPlaceholder" src="https://essucalgary.com/images/ess-logo.png"/>
-                <h2>Student Name</h2>
-                <IonText>Bio Goes Here !!</IonText>
-                <div/>
-                <IonButton className="button">Edit Profile</IonButton>
-                <div className="bottomOfPage"/>
+                <h2>{name}</h2>
+                <div className='multiline'>
+                    {bio}
+                </div>
+                <IonButton className="follow">Follow</IonButton>
                 <IonItem>Previous Posts:</IonItem>
-                <IonContent />
+                <IonContent>
+                    Posts Go Here
+                    Mohammed should do this cuz he got it set up some fancy ass way haha
+                </IonContent>
                 <IonText>[LOGO] SchulichHub</IonText>
-                <IonItem className="footer ion-margin" color="secondary"></IonItem>
+                <IonItem className="footer ion-margin" color="secondary"/>
                 <IonText className="bottomOfPage">© ESS Schulich School of Engineering U of C, 2020</IonText>
             </IonContent>
         </IonPage>
