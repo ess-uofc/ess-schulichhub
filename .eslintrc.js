bb28ec518fb56046974a1d0256dd948d10bf5424
Robert Brown <brown.rjb75@gmail.com>
2022-04-09 17:51:16 -0600

    chore: prettier fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
     extends: [
         'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
     rules: {
@@ -23,6 +22,11 @@ module.exports = {
             'warn',
             {
                 endOfLine: 'auto',
+                semi: true,
+                trailingComma: 'all',
+                singleQuote: true,
+                printWidth: 120,
+                tabWidth: 4,
                 // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
                 // e.g. "@typescript-eslint/explicit-function-return-type": "off",
             },
