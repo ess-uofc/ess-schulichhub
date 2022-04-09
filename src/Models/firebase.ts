f2edff137bc7b92e5e0e5c3ce3cb898c8f2e65d0
Robert Brown <brown.rjb75@gmail.com>
2022-04-09 17:46:43 -0600

    feat: add prettier file and fixes

--- src/Models/firebase.ts
+++ src/Models/firebase.ts
@@ -1,10 +1,10 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 // import 'firebase/auth';
-import "firebase/analytics";
+import 'firebase/analytics';
 // import 'firebase/messaging';
-import "firebase/storage";
-import FireStoreDB from "./firestore";
+import 'firebase/storage';
+import FireStoreDB from './firestore';
 export const Timestamp = firebase.firestore.Timestamp;
 export type Timestamp = firebase.firestore.Timestamp;
 export type DocumentData = firebase.firestore.DocumentData;
@@ -13,8 +13,7 @@ export type FirebaseUser = firebase.User;
 export type QuerySnapshot = firebase.firestore.QuerySnapshot;
 export type QueryDocumentSnapshot = firebase.firestore.QueryDocumentSnapshot;
 export type SnapshotOptions = firebase.firestore.SnapshotOptions;
-export type FirestoreDataConverter<T> =
-  firebase.firestore.FirestoreDataConverter<T>;
+export type FirestoreDataConverter<T> = firebase.firestore.FirestoreDataConverter<T>;
 export type SetOptions = firebase.firestore.SetOptions;
 export type DocumentReference = firebase.firestore.DocumentReference;
 export const GoogleProvider = firebase.auth.GoogleAuthProvider;
@@ -25,14 +24,14 @@ export type OAuthProvider = firebase.auth.AuthProvider;
 export const FieldValue = firebase.firestore.FieldValue;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9mulwyPhAR7tUp0MdZ31_RodyAhIMdpk",
-  authDomain: "project-hub-116d7.firebaseapp.com",
-  databaseURL: "https://project-hub-116d7.firebaseio.com",
-  projectId: "project-hub-116d7",
-  storageBucket: "project-hub-116d7.appspot.com",
-  messagingSenderId: "60966505195",
-  appId: "1:60966505195:web:c1cd5d9a1b2e1befa8ccae",
-  measurementId: "G-K08FLKLRRG",
+    apiKey: 'AIzaSyD9mulwyPhAR7tUp0MdZ31_RodyAhIMdpk',
+    authDomain: 'project-hub-116d7.firebaseapp.com',
+    databaseURL: 'https://project-hub-116d7.firebaseio.com',
+    projectId: 'project-hub-116d7',
+    storageBucket: 'project-hub-116d7.appspot.com',
+    messagingSenderId: '60966505195',
+    appId: '1:60966505195:web:c1cd5d9a1b2e1befa8ccae',
+    measurementId: 'G-K08FLKLRRG',
 };
 const app: firebase.app.App = firebase.initializeApp(firebaseConfig);
 export const analytics = app.analytics(); //UserEvent analytics such as login/creation page view etc...
@@ -40,7 +39,7 @@ export const analytics = app.analytics(); //UserEvent analytics such as login/cr
 export const FirebaseAuth = app.auth();
 export const Storage = app.storage();
 firebase.firestore().settings({
-  cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED,
+    cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED,
 });
 firebase.firestore().enablePersistence({ synchronizeTabs: true });
 export const Firestore = app.firestore();
